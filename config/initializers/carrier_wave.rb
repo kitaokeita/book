09723b03c81e71d084b53bb4708eb3ef72c9d189
北尾恵太 <kitaokeita@kitaokeita-no-MacBook-Air.local>
2017-09-24 21:40:43 +0900

    AWs error

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AKIAILG2OJNKNJ6SQQGA'],
-      :aws_secret_access_key => ENV['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']
+      :region                => ENV[''],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV[''],
+      :aws_secret_access_key => ENV['']
     }
-    config.fog_directory     =  ENV['railskitao1']
+    config.fog_directory     =  ENV['']
   end
 end
